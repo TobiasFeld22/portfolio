7af1ce08c62d75fbae155362b3a1a4d5e8fa3b27
Tobias Feld <tobias.feld@hotmail.com>
2024-04-02 14:16:12 +0200

    Add stl viewr

--- js/technology.js
+++ js/technology.js
@@ -1,3 +1,13 @@
-$(function(){
-    $("#navbar-industries").load("includes/navbar.html"); 
+$(function () {
+	$("#navbar-industries").load("includes/navbar.html");
 });
+
+function loadSTL() {
+	console.log("Loading stl's");
+	var stl_viewer = new StlViewer(document.getElementById("stl_cont"), {
+		models: [{ id: 0, filename: "../files/complete_cactus.stl" }],
+		auto_rotate: true,
+		cameray: -3,
+		cameraX: -1,
+	});
+}
