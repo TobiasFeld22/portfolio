1aad9d45b7a3ee25cab9ba0369dc39e582abf31b
Tobias Feld <tobias.feld@hotmail.com>
2024-04-04 09:32:20 +0200

    add tech website deel

--- js/technology.js
+++ js/technology.js
@@ -5,7 +5,7 @@ $(function () {
 function loadSTL() {
 	console.log("Loading stl's");
 	var stl_viewer = new StlViewer(document.getElementById("stl_cont"), {
-		models: [{ id: 0, filename: "../files/complete_cactus.stl" }],
+		models: [{ id: 0, filename: "../files/bakje.stl" }],
 		auto_rotate: true,
 		cameray: -3,
 		cameraX: -1,
